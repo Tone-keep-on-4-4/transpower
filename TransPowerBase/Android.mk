de19254fc3661792535a7b05c17bbfd48b2a3657
sjllls <2012ji123@gmail.com>
2020-03-21 14:13:40 +0800

    For Pie: LOCAL_PRIVATE_PLATFORM_APIS := true

--- TransPowerBase/Android.mk
+++ TransPowerBase/Android.mk
@@ -7,6 +7,7 @@ LOCAL_PACKAGE_NAME := TransPowerBase
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, ../common/src)
 LOCAL_RESOURCE_DIR := \
--- TransPowerSensors/Android.mk
+++ TransPowerSensors/Android.mk
@@ -7,6 +7,7 @@ LOCAL_PACKAGE_NAME := TransPowerSensors
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
